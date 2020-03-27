44065c3ab9996f1b7f69b2511e5fa981e2bd4f8b
VladimirDronik <kinord@yandex.ru>
2020-03-27 16:11:03 +0300

    usensors bugfix

--- classes/Usensors.php
+++ classes/Usensors.php
@@ -13,8 +13,13 @@ class Usensors extends Objects
      */
     static function checkI2C($idObject) {
 
-        $sql = parent::$db->query("SELECT devices.ip_address AS deviece_ip, devices.type AS device_type, devices.id AS device_id , port_SDA, port_SCL 
-                                   FROM `usensors` INNER JOIN devices ON devices.id = usensors.device_id 
+
+        $sql = parent::$db->query("SELECT devices.ip_address AS deviece_ip, devices.type AS device_type, 
+                                   devices.id AS device_id , portsSCL.num_port AS SCL , portsSDA.num_port AS SDA 
+                                   FROM `usensors` 
+                                   INNER JOIN devices ON devices.id = usensors.device_id 
+                                   INNER JOIN ports AS portsSCL ON portsSCL.id = usensors.port_SCL 
+                                   INNER JOIN ports AS portsSDA ON portsSDA.id = usensors.port_SDA 
                                    WHERE id_object = $idObject");
 
         $sensor = $sql->fetch(PDO::FETCH_OBJ);
@@ -22,15 +27,15 @@ class Usensors extends Objects
         if ($sensor->device_type == 1) {
 
             do {
-                $humidity = Megad::getI2C($sensor->device_id, $sensor->port_SDA, $sensor->port_SCL, 'htu21d', 0);
-                $temperature = Megad::getI2C($sensor->device_id, $sensor->port_SDA, $sensor->port_SCL, 'htu21d', 1);
-                $lux = Megad::getI2C($sensor->device_id, $sensor->port_SDA, $sensor->port_SCL, 'max44009', 0);
+           echo     $humidity = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'htu21d', 0);
+           echo     $temperature = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'htu21d', 1);
+                $lux = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'max44009', 0);
             } while ($humidity == 'NA' AND $temperature == 'NA' AND $lux == 'NA');
 
         } else {
 
-            define("SCL", $sensor->port_SCL);
-            define("SDA", $sensor->port_SDA);
+            define("SCL", $sensor->SCL);
+            define("SDA", $sensor->SDA);
             define("MD", "http://{$sensor->device_ip}/sec/?");
 
             // Вариант реализации I2C: 1 - полностью программный; 2 - частично аппаратный (прошивка 3.43beta1 и выше)

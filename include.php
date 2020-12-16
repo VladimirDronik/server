f2bb169e86b50a4e7a3993900105f2e25e092b31
VladimirDronik <kinord@yandex.ru>
2020-12-16 10:09:34 +0300

    cancel commit

--- include.php
+++ include.php
@@ -10,7 +10,6 @@ define("ROOT_DIR",  __DIR__);
 
 //Переменная для вывода в неё служебной информации
 $system_message = true;
-
 $remotewebsocket = '188.120.233.76:48654';
 $localwebsocket = '192.168.100.201:8000';
 $localsocket = 'tcp://127.0.0.1:5678';

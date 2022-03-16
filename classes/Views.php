b4d073770039c3b078779469d3e574757218cd59
VladimirDronik <kinord@yandex.ru>
2022-03-16 15:04:45 +0300

    extpages smallbugfix

--- classes/Views.php
+++ classes/Views.php
@@ -911,7 +911,7 @@ class Views extends System
                     $items[] = array('elements' => $elements, 'values' => $values);
 
                       $json = json_encode(array('status'=>'internalPage', 'type' => 'BoilerAuto',
-                          'idPage' => $element->intpage, 'pages' => $items));
+                          'idPage' => $page->intpage, 'pages' => $items));
 
                       echo $json;
                 return $json;
@@ -923,7 +923,7 @@ class Views extends System
                 $items[] = array('elements' => $elements, 'valuesManual' => $values);
 
                   $json = json_encode(array('status'=>'internalPage', 'type' => 'BoilerManual',
-                      'idPage' => $element->intpage, 'pages' => $items));
+                      'idPage' => $page->intpage, 'pages' => $items));
 
                   echo $json;
                   return $json;

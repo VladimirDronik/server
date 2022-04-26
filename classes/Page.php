bc0c04bf0bd9c4772e5ace1e890e0c969fa2b6aa
VladimirDronik <kinord@yandex.ru>
2022-04-26 07:26:37 +0300

    boiler modes bugfixes

--- classes/Page.php
+++ classes/Page.php
@@ -11,28 +11,37 @@ class Page extends System {
         $mode = $data_array->items[0]->mode;
         $value = $data_array->items[0]->value;
         $idPage = $data_array->items[0]->idPage;
+        $idObject = 0;
 
+        //Определяем id объекта для страницы
+        $sql = "SELECT id_object FROM elements INNER JOIN internalPages ON internalPages.idElement = elements.id
+                WHERE internalPages.id = $idPage";
+
+        if ($sql->rowCount() != 0) {
+            $element = $sql->fetch(PDO::FETCH_OBJ);
+            $idObject = $element->id_object;
+        }
 
         if ($type == 'BoilerAuto') {
             if ($mode == 'add') {
 
                 $temperatures = explode(":",$value);
 
-                parent::$db->query("INSERT INTO boiler_auto (`id`, `t_out`, `t_water`, `id_intpage`)
-                                          VALUES (null,  $temperatures[0], $temperatures[1], $idPage)");
+                parent::$db->query("INSERT INTO boiler_auto (`id`, `t_out`, `t_water`, `id_object`)
+                                          VALUES (null,  $temperatures[0], $temperatures[1], $idObject)");
 
 
             } elseif ($mode == 'del') {
 
-                parent::$db->query("DELETE FROM boiler_auto WHERE id_intpage = $idPage AND t_out = $value");
+                parent::$db->query("DELETE FROM boiler_auto WHERE id_object = $idObject AND t_out = $value");
 
 
             }
         } else { //Если manual выбрано
 
             parent::$db->exec("UPDATE boiler_manual SET `set_value` = $value
-                                       WHERE `id` = $idPage");
-            
+                                       WHERE `id_object` = $idObject");
+
         }
 
     }

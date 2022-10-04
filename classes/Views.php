8ab49cebf1829b8a0af302d9f062d46a8edf88e5
s044316 <kinordikus@gmail.com>
2022-10-04 12:40:00 +0300

    graph_counts bugfix

--- classes/Views.php
+++ classes/Views.php
@@ -767,7 +767,7 @@ class Views extends System
 
     public function getCountsGraphs($idCount, $period) {
 
-        if ($period == 'month')
+         if (($period == 'month')||($period == '(period)'))
         $sql = "SELECT DATE_FORMAT(datetime,'%d') AS date, SUM(value) AS value FROM `graph_counts` 
                 WHERE `datetime` >= NOW() - INTERVAL 30 DAY AND id_count = $idCount
                 GROUP BY DAY(datetime)  ORDER BY datetime";

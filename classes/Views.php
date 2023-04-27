8ca8ae305fbaad9a8c861ef68c92ffaa3ca80af8
VladimirDronik <kinord@yandex.ru>
2023-04-27 18:35:52 +0300

    struct bugfix

--- classes/Views.php
+++ classes/Views.php
@@ -1057,8 +1057,8 @@ class Views extends System
                 'operation' => $conditioner->operation,
                 'fan' => $conditioner->fan,
                 'precision' => $conditioner->prec,
-                'operation_modes' => $conditioner->operation,
-                'fan_modes' => $conditioner->fan,
+                'operation_modes' => $conditioner->operationModes,
+                'fan_modes' => $conditioner->fanModes,
                 'min' => $conditioner->min,
                 'max' => $conditioner->max
           );

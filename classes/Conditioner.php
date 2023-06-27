8a992232244b8a045a971fbafdfeb510b22f979f
VladimirDronik <kinord@yandex.ru>
2023-06-27 14:28:41 +0300

    conditioner object bugfix

--- classes/Conditioner.php
+++ classes/Conditioner.php
@@ -23,12 +23,12 @@ class Conditioner extends Device
 
 
         if ($state == 'ON' ) {
-            $object->setStatus('ON');
+            $object->setStatus('ON', true, false);
             $query = 'SELECT `code`, conditioners.wb_mir AS modbus_addr, devices.ip_address AS ip_addr FROM `conditioner_codes` INNER JOIN `conditioner_models` ON conditioner_codes.kind = conditioner_models.kind INNER JOIN `conditioners` ON conditioners.model = conditioner_models.id  
                 INNER JOIN devices ON devices.id = conditioners.device_id
                 WHERE `conditioner_codes.temperature` = '.$temperature.' AND `conditioner_codes.operationMode` = "'.$oper.'" AND `conditioner_codes.fanMode` = "'.$fan.'" AND conditioners.id_object = '.self::$idObject;
          } else {
-            $object->setStatus('OFF');
+            $object->setStatus('OFF', true, false);
             $query = 'SELECT `code`, conditioners.wb_mir AS modbus_addr, devices.ip_address AS ip_addr FROM `conditioner_codes` INNER JOIN `conditioner_models` ON conditioner_codes.kind = conditioner_models.kind INNER JOIN `conditioners` ON conditioners.model = conditioner_models.id  
                 INNER JOIN devices ON devices.id = conditioners.device_id
                 WHERE conditioner_codes.status = "'.$state.'" AND conditioners.id_object = '.self::$idObject;
@@ -46,6 +46,7 @@ class Conditioner extends Device
             parent::$db->query("UPDATE conditioners SET 
                                 `temp` = $temperature, `state` = $state, `operationMode`= $oper, `fanMode` = $fan   
                                 WHERE id_object =".self::$idObject);
+
 
         //Отправляем команду скрипту кондиционера        
         exec('cd /usr/bin && ./rs_control ir_raw -d wb-mir --ip '.$conditioner->ip_addr.' -u '.$conditioner->modbus_addr.' --ir_signal "'.$conditioner->code.'"');

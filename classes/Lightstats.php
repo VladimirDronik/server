a33cfb60845ba4414ba9d4d59efe87c81d679cc3
Vitaliy Nikolaev <vn@touchon.tech>
2023-07-31 22:05:15 +0300

    Исправлены методы светостата, добавлена проверка на выход значения за установленные пределы

--- classes/Lightstats.php
+++ classes/Lightstats.php
@@ -7,59 +7,41 @@
 
 class Lightstats extends Objects
 {
-
-    private $script;
-    private $id_lightstat;
-
-    private $min_threshold;
-    private $max_threshold;
-    private $min_alarm;
-    private $max_alarm;
-    private $lightstat;
-    private $idObject;
-    private $typeObject;
-    private $placetype;
-    private $usensor;
-    private $name;
-
+    private static $lightstat = null;
 
     /**
      * Конструктор определяет рабочие параметры у выбранного светостата
      *
      * @param int $id_lightstat
      */
-    function __construct($idObjectLightstat=null)
+    function __construct($idObject=null)
     {
-
-        if($idObjectLightstat!=null) {
-            $this->script = new Scripts();
-
-
+        if($idObject!=null)
+        {
             //Получаем все данные светостата
-            $scriptsql = parent::$db->query("SELECT  lightstats.id AS id, current, optimal, gisteresis, mode, object, method_on, 
-                                            method_off, `min_threshold`, `max_threshold`, `min_alarm`, `max_alarm`, `objects`.`type` as `type_object`,
-                                            `placetype`, `usensor_id`, lightstats.`name`
-                                            FROM lightstats 
-                                            INNER JOIN objects ON  id_object=objects.id
-                                            WHERE id_object=$idObjectLightstat");
+            $sql = parent::$db->query("SELECT lightstats.id_object AS idObject,
+                                              lightstats.id AS lighstat_id,
+                                              current,
+                                              optimal,
+                                              gisteresis,
+                                              mode,
+                                              object,
+                                              method_on,
+                                              method_off,
+                                              `min_threshold`,
+                                              `max_threshold`,
+                                              `min_alarm`,
+                                              `max_alarm`,
+                                              `objects`.`type` as `type_object`,
+                                              `placetype`,
+                                              `usensor_id`,
+                                              lightstats.`name`
+                                       FROM lightstats 
+                                       INNER JOIN objects ON  id_object=objects.id
+                                       WHERE id_object=$idObject");
 
-
-
-            $this->lightstat = $lightstat = $scriptsql->fetch(PDO::FETCH_OBJ);
-
-            $this->idObject = $idObjectLightstat;
-            $this->id_lightstat = $lightstat->id;
-            $this->min_threshold = $lightstat->min_threshold;
-            $this->max_threshold = $lightstat->max_threshold;
-            $this->min_alarm = $lightstat->min_alarm;
-            $this->max_alarm = $lightstat->max_alarm;
-            $this->typeObject = $lightstat->type_object;
-            $this->placetype = $lightstat->placetype;
-            $this->usensor = $lightstat->usensor_id;
-            $this->name = $lightstat->name;
-
+            self::$lightstat = $sql->fetch(PDO::FETCH_OBJ);
         }
-
     }
 
 
@@ -72,48 +54,47 @@ class Lightstats extends Objects
 
     function check()
     {
+        $lightstat = self::$lightstat;
 
         //Если светостат с реакцией на посветление
-        if ($this->lightstat->mode == 1)
+        if ($lightstat->mode == 1)
         {
 
-            if ($this->lightstat->current >=($this->lightstat->optimal))
-            {
-                // Вызываем метод off
-                if($this->lightstat->method_off)
-                Action::runAction($this->lightstat->method_off, 'lightstat', $this->idObject);
-                return 0;
-
-            }
-
-            if ($this->lightstat->current < ($this->lightstat->optimal-$this->lightstat->gisteresis))
+            if ($lightstat->current >= ($lightstat->optimal+$lightstat->gisteresis/2))
             {
                 // Вызываем метод on
-                if($this->lightstat->method_on)
-                Action::runAction($this->lightstat->method_on, 'lightstat', $this->idObject);
+                if($lightstat->method_on)
+                Action::runAction($lightstat->method_on, 'lightstat', $lightstat->idObject);
                 return 1;
             }
 
-
-        } else //Если светостат с реакцией на потемнение
-        {
-            if ($this->lightstat->current <=($this->lightstat->optimal-$this->lightstat->gisteresis))
+            if ($lightstat->current < ($lightstat->optimal-$lightstat->gisteresis/2))
             {
                 // Вызываем метод off
-                if($this->lightstat->method_off)
-                Action::runAction($this->lightstat->method_off, 'lightstat', $this->idObject);
+                if($lightstat->method_off)
+                Action::runAction($lightstat->method_off, 'lightstat', $lightstat->idObject);
                 return 0;
             }
 
 
-            if ($this->lightstat->current > $this->lightstat->optimal)
+        } 
+        else //Если светостат с реакцией на потемнение
+        {
+            if ($lightstat->current <= ($lightstat->optimal-$lightstat->gisteresis/2))
             {
                 // Вызываем метод on
-                if($this->lightstat->method_on)
-                Action::runAction($this->lightstat->method_on, 'lightstat', $this->idObject);
+                if($lightstat->method_on)
+                Action::runAction($lightstat->method_on, 'lightstat', $lightstat->idObject);
                 return 1;
             }
 
+            if ($lightstat->current > $lightstat->optimal+$lightstat->gisteresis/2)
+            {
+                // Вызываем метод off
+                if($lightstat->method_off)
+                Action::runAction($lightstat->method_off, 'lightstat', $lightstat->idObject);
+                return 0;
+            }
         }
     }
 
@@ -125,82 +106,47 @@ class Lightstats extends Objects
      */
     function getLux()
     {
-        $alarm_cnt = 0;
-
-
-        if($this->placetype == 'port') {
-
-
-
+        $lightstat = self::$lightstat;
+        $error = false;
+        
+        if($lightstat->placetype == 'port') 
+        {
             //Ищем к какому порту и устройству принадлежит светостат
-            $lightstatsql = parent::$db->query("SELECT lightstats.id_object AS id_object,
-                                                  ports_SDA.num_port AS SDA, ports_SCL.num_port AS SCL, lightstats.port_SCL, lightstats.`name`,
-                                                  devices.ip_address AS ip
-                                              FROM lightstats     
-                                              INNER JOIN ports AS ports_SDA
-                                              ON ports_SDA.id = lightstats.port_SDA
-                                              INNER JOIN ports AS ports_SCL
-                                              ON ports_SCL.id = lightstats.port_SCL
-                                              INNER JOIN devices
-                                              ON  ports_SDA.id_device = devices.id
-                                              WHERE lightstats.id=$this->id_lightstat");
+            $sql = parent::$db->query("SELECT ports_SDA.num_port AS SDA,
+                                              ports_SCL.num_port AS SCL,
+                                              devices.id AS device_id
+                                       FROM lightstats     
+                                       INNER JOIN ports AS ports_SDA ON ports_SDA.id = lightstats.port_SDA
+                                       INNER JOIN ports AS ports_SCL ON ports_SCL.id = lightstats.port_SCL
+                                       INNER JOIN devices ON ports_SDA.id_device = devices.id
+                                       WHERE lightstats.id_object = $lightstat->idObject");
 
-            $lightstat = $lightstatsql->fetch(PDO::FETCH_OBJ);
-
-            do {
-
-                do {
-
-                        define("SCL", $lightstat->SCL);
-                        define("SDA", $lightstat->SDA);
-                        define("MD", "http://{$lightstat->ip}/sec/?");
-
-                        // Вариант реализации I2C: 1 - полностью программный; 2 - частично аппаратный (прошивка 3.43beta1 и выше)
-                        define("V", "2");
-
-                      $lux = get_lux1750();
-
-                    $alarm_cnt++;
-
-                    //Если превышено число аварийных значений светостата
-                    if ($alarm_cnt >= 10) {
-                        //Здесь сделать вызов обработчика аварии и выходим из цикла
-                        System::addLog('error', 'Светостат "' . $this->name . '" не доступен', 'sensor');
-
-
-                        $error = true;
-                        break 2;
-                    }
-
-                    //Проверка на аварийные занчения
-                } while (($lux < $this->min_alarm) || ($lux > $this->max_alarm));
-
-                //Проверка на пороговые значения
-            } while (($lux < $this->min_threshold) || ($lux > $this->max_threshold));
-
-        } else { //Светостат входит в состав унивесального датчика
-
-            $result = Usensors::checkI2C($this->usensor);
+            $lightstat_i2c = $sql->fetch(PDO::FETCH_OBJ);
+            $lux = Megad::getI2C($lightstat_i2c->device_id, $lightstat_i2c->SDA, $lightstat_i2c->SCL, 'bh1750');
+        } 
+        else 
+        { 
+            //Светостат входит в состав унивесального датчика
+            $result = Usensors::checkI2C($lightstat->usensor_id);
             $lux = $result['lux'];
-
         }
 
+        $error = self::validateValue($lux);
 
-        //TODO: проверка на слишком резкое изменеие значения
 
-        if (!$error) {
+        if (!$error)
+        {
             //Заносим значение светостата в БД в таблицу светостатов и в таблицу графиков
-            parent::$db->query("UPDATE lightstats SET `current` = $lux
-                                         WHERE id=$this->id_lightstat");
-
-            Graphs::insertToLightstats($this->id_lightstat, $lux);
+            parent::$db->query("UPDATE lightstats SET `current` = $lux WHERE `id_object` = $lightstat->idObject");      
+            Graphs::insertToLightstats($lightstat->lighstat_id, $lux);
         }
 
         //Отдаем значение визуальному компоненту
-        $sql = parent::$db->query("SELECT id FROM `view_items` WHERE `id_object`= $this->idObject");
+        $sql = parent::$db->query("SELECT id FROM `view_items` WHERE `id_object`= $lightstat->idObject");
         $viewItem = $sql->fetch(PDO::FETCH_OBJ);
 
-        if($viewItem->id) {
+        if(isset($viewItem->id))
+        {
             $view = new Views();
             $view->updateItem($viewItem->id);
         }
@@ -213,11 +159,45 @@ class Lightstats extends Objects
      */
     public static function getValueFromDB($idLightstat)
     {
-        $lightstatsql = parent::$db->query("SELECT `current` FROM lightstats WHERE id_object = $idLightstat");
+        $sql = parent::$db->query("SELECT `current` FROM lightstats WHERE id_object = $idLightstat");
         if($lightstat = $lightstatsql->fetch(PDO::FETCH_OBJ));
         return $lightstat->current;
-
     }
 
+    /**
+     * Проверка значения на ошибки
+     */
+    private static function validateValue ($lux)
+    {
+        $lightstat = self::$lightstat;
 
+        if ($lux > $lightstat->max_alarm) 
+        {
+            System::addLog('error', 
+                'Светостат "'.$lightstat->name.'" (ID '.$lightstat->idObject.
+                '). Значение '.$lightstat->current.' ед. выше аварийного порога.',
+                'sensor');
+            $error=false;
+        }
+        
+        if ($lux < $lightstat->min_alarm)
+        {
+            System::addLog('error', 
+                'Светостат "'.$lightstat->name.'" (ID '.$lightstat->idObject.
+                '). Значение '.$lightstat->current.' ед. ниже аварийного порога.',
+                'sensor');
+            $error=false;
+        }
+        
+
+        if (($lux < $lightstat->min_threshold) || ($lux > $lightstat->max_threshold))
+        {
+            System::addLog('error', 
+                'Светостат "'.$lightstat->name.'" (ID '.$lightstat->idObject.
+                '). Значение '.$lightstat->current.' выходит за пределы измерения.',
+                'sensor');
+            $error=true;
+        }
+        return $error;
+    }
 }

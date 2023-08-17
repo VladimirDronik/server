cc3fa6bbb70c58567ae3cf3daa96997f98654f34
Vitaliy Nikolaev <vn@touchon.tech>
2023-08-17 12:22:21 +0300

    Добавлен датчик SCD4x

--- classes/Usensors.php
+++ classes/Usensors.php
@@ -18,7 +18,8 @@ class Usensors extends Objects
                                     devices.type AS device_type,
                                     devices.id AS device_id,
                                     portsSCL.num_port AS SCL,
-                                    portsSDA.num_port AS SDA
+                                    portsSDA.num_port AS SDA,
+                                    usensors.type AS sensor_type
                                     FROM `usensors` 
                                     INNER JOIN devices ON devices.id = usensors.device_id 
                                     INNER JOIN ports AS portsSCL ON portsSCL.id = usensors.port_SCL 
@@ -27,35 +28,63 @@ class Usensors extends Objects
 
         $sensor = $sql->fetch(PDO::FETCH_OBJ);
 
-        $humidity = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'htu21d');
-        if (preg_match("/[a-z]/i", $humidity)) $humidity = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280', 2);
-        $temperature = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'htu21d', 1);
-        if (preg_match("/[a-z]/i", $temperature)) $temperature = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280', 1);
-        $lux = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bh1750');         
-        $atm_pressure = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280');
+        switch ($sensor->sensor_type)
+        {
+            case 'bh1750' :
+                $lux = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bh1750');
+                break;
+
+            case 'htu21d' :
+                $humidity = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'htu21d');
+                $temperature = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'htu21d', 1);
+                break;
+
+            case 'bme280' :
+                $humidity = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280', 2);
+                $temperature = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280', 1);
+                $atm_pressure = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280');
+                break;
+
+            case 'outdoorv2':
+                $humidity = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'htu21d');
+                $temperature = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'htu21d', 1);
+                $lux = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bh1750');
+                break;
+
+            case 'outdoorv3' :
+                $humidity = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280', 2);
+                $temperature = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280', 1);
+                $atm_pressure = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bmx280');
+                $lux = Megad::getI2C($sensor->device_id, $sensor->SDA, $sensor->SCL, 'bh1750');
+                break;
+
+            case 'scd40' :
+            case 'scd41' :
+                $co2 = Megad::status($sensor->SDA, 'get', $sensor->device_id, 0);
+                $temperature = Megad::status($sensor->SDA, 'get', $sensor->device_id, 1);
+                $humidity = Megad::status($sensor->SDA, 'get', $sensor->device_id, 2);
+                break;
+        }
 
         //Проверка на наличие значения
+        if (!isset($temperature)) $temperature='NULL';
         if(preg_match("/[a-z]/i", $temperature)) $temperature='NULL';
+        if (!isset($humidity)) $humidity='NULL';
         if(preg_match("/[a-z]/i", $humidity)) $humidity='NULL';
+        if (!isset($lux)) $lux='NULL';
         if(preg_match("/[a-z]/i", $lux)) $lux='NULL';
+        if (!isset($atm_pressure)) $atm_pressure='NULL';
         if(preg_match("/[a-z]/i", $atm_pressure)) $atm_pressure='NULL';
         
         //Добавляем данные в таблицу унивесального датчика
-        if( $result = parent::$db->query("SELECT * FROM usensors")  &&  isset($result['atm_pressure']) ){
-            parent::$db->exec("UPDATE `usensors` SET
-                            `temp` = $temperature,
-                            `hum` = $humidity,
-                            `lux` = $lux,
-                            `atm_pressure` = $atm_pressure
-                            WHERE `id_object` = $idObject");
-        }else{
-            parent::$db->exec("UPDATE `usensors` SET
-                            `temp` = $temperature,
-                            `hum` = $humidity,
-                            `lux` = $lux
-                            WHERE `id_object` = $idObject");
-        }
+        parent::$db->exec("UPDATE `usensors` 
+                           SET `temp` = $temperature,
+                               `hum` = $humidity,
+                               `lux` = $lux,
+                               `atm_pressure` = $atm_pressure,
+                               `co2` = $co2
+                           WHERE `id_object` = $idObject");
 
-        return ['temp' => $temperature, 'hum' => $humidity, 'lux' => $lux, 'atm_pressure' => $atm_pressure];
+        return ['temp' => $temperature, 'hum' => $humidity, 'lux' => $lux, 'atm_pressure' => $atm_pressure, 'co2' => $co2];
     }
 }

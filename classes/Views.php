d3ae50104829830b7df0c232c8f704feb9f5c62b
VladimirDronik <kinord@yandex.ru>
2023-09-11 17:53:52 +0300

    LED bugfix

--- classes/Views.php
+++ classes/Views.php
@@ -1111,7 +1111,7 @@ class Views extends System
      * Функия отдает параметры RGB ленты
      */
     public function getTape($idTape) {
-        $sql = parent::$db->query("SELECT tapes.id, h, s, v, type, status FROM tapes
+        $sql = parent::$db->query("SELECT tapes.id, h, s, v, tapes.type, tapes.status FROM tapes
          INNER JOIN objects ON objects.id = tapes.id_object 
          INNER JOIN view_items ON view_items.id_object = objects.id 
          WHERE view_items.id = $idTape");

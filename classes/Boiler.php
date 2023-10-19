c28c5a753821270db667149bb927702074dd68ff
VladimirDronik <kinord@yandex.ru>
2023-10-19 15:58:08 +0300

    adding nevoton for  openterm

--- classes/Boiler.php
+++ classes/Boiler.php
@@ -12,6 +12,15 @@ class Boiler extends System
     {
         $sql = parent::$db->query("SELECT * FROM boiler WHERE `id_object` = $idObject");
         $this->boiler = $sql->fetch(PDO::FETCH_OBJ);
+
+        //Определяем параметры контроллера бойлера
+        $sql = parent::$db->query("SELECT devtypes.nam AS type_controller, devices.ip_address AS address, devices.port AS port FROM devtypes 
+                                   INNER JOIN devices ON devtypes.id = devices.type WHERE devices.id = {$this->boiler->id_controller}");
+        $boiler_controller = $sql->fetch(PDO::FETCH_OBJ);
+        $this->boiler->type_controller = $boiler_controller->type_controller;
+        $this->boiler->address_controller = $boiler_controller->address;
+        $this->boiler->port = $boiler_controller->port;
+
     }
 
     /**
@@ -47,6 +56,21 @@ class Boiler extends System
         $water_temp = '[{"status":"'.$this->boiler->water_temp.'°С", "settings": "true"}]';
         parent::$db->exec("UPDATE elements SET `value` = '$water_temp' 
                                    WHERE `id_object` = {$this->boiler->id_object} AND handle = 'water_temp'");
+
+        //Обновление уличной температуры
+        $outdoor_temp = '[{"status":"'.$this->boiler->outdoor_temp.'°С"]';
+        parent::$db->exec("UPDATE elements SET `value` = '$outdoor_temp' 
+                                   WHERE `id_object` = {$this->boiler->id_object} AND handle = 'outdoor_temp'");
+
+        //Обновление кода ошибки
+        $error_code = '[{"status":"'.$this->boiler->error_code.'"]';
+            parent::$db->exec("UPDATE elements SET `value` = '$error_code' 
+                                       WHERE `id_object` = {$this->boiler->id_object} AND handle = 'error_code'");
+
+        //Описание расширенной ошибки, если есть
+        $ext_error = '[{"status":"'.$this->boiler->ext_error.'"]';
+            parent::$db->exec("UPDATE elements SET `value` = '$ext_error' 
+                                       WHERE `id_object` = {$this->boiler->id_object} AND handle = 'ext_error'");
 
 
         //Обновление значения статуса котла для всех элементов с таким типом
@@ -137,7 +161,7 @@ class Boiler extends System
 
             //Ищем страницу для котла по id объекта
             // Смотрим температуру на улице через привязанный датчик температуры и выставляем
-            // температуру котла в соответствии с таблицей
+            // температуру котла в соответствии с таблицей соответствия температур
             $sql = parent::$db->query("SELECT boiler_auto.t_water,  boiler_auto.t_out, termostats.current FROM boiler_auto 
                                    INNER JOIN boiler ON boiler.id_object = boiler_auto.id_object
                                    INNER JOIN termostats ON termostats.id_object = boiler.id_outside_thermostat
@@ -150,40 +174,20 @@ class Boiler extends System
             $this->boiler->target_heat_temp = $boiler_autoparams->t_water;
 
         } else {
-            // Если ручной режим, то у котла устанавливаем температуру, которая указана в таблице
+            // Если ручной режим, то у котла устанавливаем температуру, которая указана в таблице ручного режима
             $sql = parent::$db->query("SELECT set_value FROM boiler_manual WHERE id_object = {$this->boiler->id_object}");
             $boiler_manualparams = $sql->fetch(PDO::FETCH_OBJ);
             $this->boiler->target_heat_temp = $boiler_manualparams->set_value;
         }
 
-
-        //ОТправляем данные из БД на котел
-        if ($this->boiler->thermostat == 1) $cmd = 'on';
-        else $cmd = 'off';
-
-        file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=$cmd");
-
-        if($this->boiler->boiler == 0) $boiler = 'off';
-        else $boiler = 'on';
-
-        file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=set&heat={$this->boiler->target_heat_temp}".
-            "&water={$this->boiler->target_water_temp}&boiler=$boiler");
-
-
-        //Опрашиваем котёл, заносим параметры в переменные и в таблицу котла
-        $stateBoilerResponse = file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=get");
-
-        $stateBoiler = json_decode($stateBoilerResponse);
-
-        $this->boiler->feed_heat_temp = $stateBoiler->feed_heat_temp;
-        $this->boiler->back_heat_temp = $stateBoiler->back_heat_temp;
-        $this->boiler->target_heat_temp = $stateBoiler->target_heat_temp;
-        $this->boiler->target_water_temp = $stateBoiler->target_water_temp;
-        $this->boiler->thermostat = $stateBoiler->thermostat;
-        $this->boiler->boiler = $stateBoiler->boiler;
-        $this->boiler->water_temp = $stateBoiler->water_temp;
-        $this->boiler->feed_water_temp = $stateBoiler->feed_water_temp;
-        $this->boiler->pressure = round($stateBoiler->pressure/1000,1);
+        
+        if ($this->boiler->type_controller == "nevoton") {
+            $this->sendDataToNevoton(); 
+            $this->reqDataFromNevoton();
+        } else {
+            $this->sendDataToTouchonEbus();
+            $this->reqDataFromTouchonEbus();
+        }
 
 
 
@@ -195,6 +199,9 @@ class Boiler extends System
                                 `boiler` =  {$this->boiler->boiler},
                                 `water_temp` = {$this->boiler->water_temp},
                                 `feed_water_temp` = {$this->boiler->feed_water_temp},
+                                `outsoor_temp` = {$this->boiler->outdoor_temp},
+                                `error_code` =  {$this->boiler->error_code},
+                                `ext_error` =  {$this->boiler->ext_error},
                                 `pressure` = {$this->boiler->pressure}
                                   WHERE `id_object` = {$this->boiler->id_object}");
 
@@ -205,14 +212,26 @@ class Boiler extends System
 
     //Установить температуру отопления для котла
     public function setHeat(int $temperature) {
+        if (!$this->boiler->lock) {
+        $this->boiler->target_heat_temp = $temperature;
+        parent::$db->exec("UPDATE boiler SET `target_heat_temp` = {$temperature}
+                              WHERE `id_object` = {$this->boiler->id_object}");
 
-        if(!$this->boiler->lock) {
-            $this->boiler->target_heat_temp = $temperature;
-            parent::$db->exec("UPDATE boiler SET `target_heat_temp` = {$temperature}
-                                  WHERE `id_object` = {$this->boiler->id_object}");
+        if ($this->boiler->type_controller == "nevoton") {
+            
+            $modbus = $this->modbusSetup();
 
+            //включение систем котла
+            $result = $modbus->sendQuery(9, 1, "03E7", 1);
+            //Включение ручного режима (отключение термостата)
+            $modbus->sendQuery(9, 1, "03EC", 1);
+             //Запись температуры котла
+            $modbus->sendQuery($this->boiler->address, 6, "0x03FA", $this->boiler->target_heat_temp);
+
+        }else {
             file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=set&heat=$temperature}");
-        }
+    }
+    }
 
     }
 
@@ -220,16 +239,28 @@ class Boiler extends System
     //Установить температуру отопления для воды
     public function setWater(int $temperature)
     {
-
         if (!$this->boiler->lock) {
+        $this->boiler->target_water_temp = $temperature;
+        parent::$db->exec("UPDATE boiler SET `target_water_temp` = {$temperature}
+                              WHERE `id_object` = {$this->boiler->id_object}");
 
-            $this->boiler->target_water_temp = $temperature;
-            parent::$db->exec("UPDATE boiler SET `target_water_temp` = {$temperature}
-                                  WHERE `id_object` = {$this->boiler->id_object}");
+        if ($this->boiler->type_controller == "nevoton") {
+          
+            $modbus = $this->modbusSetup();
 
-            file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=set&water=$temperature}");
-
+            //включение систем котла
+            $result = $modbus->sendQuery(9, 1, "03E7", 1);
+            //Включение ручного режима (отключение термостата)
+            $modbus->sendQuery(9, 1, "03EC", 1);
+             //Запись температуры ГВС
+             $modbus->sendQuery($this->boiler->address, 6, "0x0400", $this->boiler->target_water_temp);
+        } else {
+            
+                file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=set&water=$temperature}");    
+            
         }
+    }
+       
     }
 
 
@@ -297,5 +328,120 @@ class Boiler extends System
                                        WHERE `id_object` = $id_object");
     }
 
+
+
+    // Отправка данных на котел ebus
+    private function sendDataToTouchonEbus() {
+
+        //Отправляем данные из БД на котел
+        if ($this->boiler->thermostat == 1) $cmd = 'on';
+        else $cmd = 'off';
+
+        file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=$cmd");
+
+        if($this->boiler->boiler == 0) $boiler = 'off';
+        else $boiler = 'on';
+
+        file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=set&heat={$this->boiler->target_heat_temp}".
+            "&water={$this->boiler->target_water_temp}&boiler=$boiler");
+
+    }
+
+    // опрос котла ebus
+    private function reqDataFromTouchonEbus() {
+
+         //Опрашиваем котёл, заносим параметры в переменные и в таблицу котла
+         $stateBoilerResponse = file_get_contents("http://{$this->boiler->ip_address}/thermostat?cmd=get");
+
+         $stateBoiler = json_decode($stateBoilerResponse);
+ 
+         $this->boiler->feed_heat_temp = $stateBoiler->feed_heat_temp;
+         $this->boiler->back_heat_temp = $stateBoiler->back_heat_temp;
+         $this->boiler->target_heat_temp = $stateBoiler->target_heat_temp;
+         $this->boiler->target_water_temp = $stateBoiler->target_water_temp;
+         $this->boiler->thermostat = $stateBoiler->thermostat;
+         $this->boiler->boiler = $stateBoiler->boiler;
+         $this->boiler->water_temp = $stateBoiler->water_temp;
+         $this->boiler->feed_water_temp = $stateBoiler->feed_water_temp;
+         $this->boiler->pressure = round($stateBoiler->pressure/1000,1);
+    }
+
+
+    // Отправка данных на котел Невотон
+    private function sendDataToNevoton() {
+
+        $modbus = $this->modbusSetup();
+
+        //Включение ручного режима (отключение термостата)
+        $modbus->sendQuery(9, 1, "03EC", 1);
+        //Запись температуры котла
+        $modbus->sendQuery($this->boiler->address, 6, "0x03FA", $this->boiler->target_heat_temp);
+        //Запись температуры ГВС
+        $modbus->sendQuery($this->boiler->address, 6, "0x0400", $this->boiler->target_water_temp);
+    }
+
+    // Опрос котла Невотон
+    private function reqDataFromNevoton() {
+
+      $modbus = $this->modbusSetup();
+
+        //модуляция горелки
+        $this->boiler->flame = $modbus->sendQuery($this->boiler->address, 3, "03EF", 1);
+        //давление
+        $this->boiler->pressure = $modbus->sendQuery($this->boiler->address, 3, "03F0", 1);
+        //скорость потока ГВС
+        $this->boiler->GVS_flow_rate = $modbus->sendQuery($this->boiler->address, 3, "03F1", 1);
+        //температура котла
+        $this->boiler->feed_heat_temp = $modbus->sendQuery($this->boiler->address, 3, "03F2", 1);
+        //температура ГВС
+        $this->boiler->water_temp = $modbus->sendQuery($this->boiler->address, 3, "03F3", 1);
+        //Внешняя температура
+        $this->boiler->outdoor_temp = $modbus->sendQuery($this->boiler->address, 3, "03F4", 1);
+        
+        //считываение признака ошибки
+        $err = $modbus->sendQuery($this->boiler->address, 1, "03F9", 1);
+        if ($err != null) {
+            //Если признак ошибки есть, то записываем код ошибки
+            $this->boiler->error_code =  $modbus->sendQuery($this->boiler->address, 3, "03F6", 1);
+            //Если у котла есть функция получения расширенной ошибки
+            $ext_err_flag = $modbus->sendQuery($this->boiler->address, 1, "03FA", 1);
+            if ( $ext_err_flag != null) {
+                
+                if ($modbus->sendQuery($this->boiler->address, 1, "03FB", 1) == 1)
+                $this->boiler->ext_error = "Ошибка воздушного давления";
+
+                if ($modbus->sendQuery($this->boiler->address, 1, "3FC", 1) == 1)
+                $this->boiler->ext_error = "Ошибка по газу/пламени";
+
+                if ($modbus->sendQuery($this->boiler->address, 1, "3FD", 1) == 1)
+                $this->boiler->ext_error = "Блокировка внешнего управления";
+
+                if ($modbus->sendQuery($this->boiler->address, 1, "3FE", 1) == 1)
+                $this->boiler->ext_error = "Ошибка низкого давления воды";
+
+                if ($modbus->sendQuery($this->boiler->address, 1, "3FF", 1) == 1)
+                $this->boiler->ext_error = " Необходимо внешнее обслуживание";
+
+                if ($modbus->sendQuery($this->boiler->address, 1, "400", 1) == 1)
+                $this->boiler->ext_error = "Ошибка превышения максимальной температуры воды";
+
+            }
+        }
+
+    }
+
+    //Установка параметров работы модбас
+    private function modbusSetup() {
+        $modbus = new PhpSerialModbus;
+
+        if ($this->boiler->port == 0 ) $pt = '/dev/ttyUSB0';
+        else $pt = '/dev/ttyUSB1'; 
+
+        $modbus->deviceInit($pt, 9600, 'none', 8, 1, 'none');
+        $modbus->deviceOpen();
+        $modbus->debug = true;
+
+        return $modbus;
+    }
 
 }
